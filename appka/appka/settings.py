ac166c333be5ff840ddeafe5fc17555c7db03ef2
Robert <robert.czwartosz@tlen.pl>
2020-10-13 17:09:05 +0200

    update

--- appka/appka/settings.py
+++ appka/appka/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b2xa9duz71zyp^!_!+r-%w(gycjxh25dn(81c7b@+6-%ooo!$0'
+SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
